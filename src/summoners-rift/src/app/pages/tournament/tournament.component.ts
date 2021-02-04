7669ca2651bafd43774c0df680b3a76bd409b3af
Denner Fernandes <dev.denner@outlook.com>
2021-02-04 00:16:21 -0300

    feat: add tournament.component
    Signed-off-by: Denner Fernandes <dev.denner@outlook.com>

---
+++ src/summoners-rift/src/app/pages/tournament/tournament.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { ChallengersService } from './../../services/challengers/challengers.service';
+import { TournamentService } from './../../services/tournament/tournament.service';
+import { IChallengers } from './../../services/challengers/IChallengers';
+
+
+@Component({
+  selector: 'app-tournament',
+  templateUrl: './tournament.component.html',
+  styleUrls: ['./tournament.component.scss']
+})
+export class TournamentComponent implements OnInit {
+
+  public challengers: IChallengers[];
+  public matches: any = {};
+
+  constructor(
+    private tournamentService: TournamentService,
+    private challengersService: ChallengersService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    if (!this.tournamentService.getStatusTournament()) {
+      this.router.navigateByUrl('/home');
+    }
+
+    this.challengers = this.shuffle(this.challengersService.getChallengers());
+    this.matches = this.challengersDraw();
+  }
+
+  shuffle(array: IChallengers[]): IChallengers[] {
+    let currentIndex = array.length;
+    let temporaryValue: any;
+    let randomIndex: number;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  challengersDraw(): any {
+    return {
+      one: [
+        this.challengers[0],
+        this.challengers[1],
+      ],
+      two: [
+        this.challengers[2],
+        this.challengers[3],
+      ],
+      three: [
+        this.challengers[4],
+        this.challengers[5],
+      ],
+      four: [
+        this.challengers[6],
+        this.challengers[7],
+      ],
+      five: [],
+      six: [],
+      seven: [],
+    };
+  }
+
+  roundChampion(challenger: any, match: string, position: number): void {
+    if (match === 'win') {
+      return;
+    }
+    this.matches[match][position] = challenger;
+    console.log('this.matches[match]', this.matches[match])
+  }
+
+}
